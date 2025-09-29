6e9d4ecef231da9c3bf61e60905e3de617f20459
alaa7041 <a.hassan7041@su.edu.eg>
2025-09-29 21:26:03 +0300

    create fisrt styles of fonts

--- lib/core/theme/fonts.dart
+++ lib/core/theme/fonts.dart
@@ -1,3 +1,23 @@
+import 'dart:ui';
+
+import 'package:test_app/core/theme/colors.dart';
+
 class Styles{
-  static const String poppins = 'Poppins';
+  static const String fontFamily = 'Raleway';
+  static const Color fontColor = ColorsManager.homeTextColor;
+
+    static TextStyle font8Regular() => TextStyle(
+        fontSize: 8,
+        fontWeight: FontWeight.w400,
+        color: fontColor,
+        fontFamily: fontFamily,
+      );
+
+    static TextStyle font12Regular() => TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        color: fontColor,
+        fontFamily: fontFamily,
+      );
+
 }
