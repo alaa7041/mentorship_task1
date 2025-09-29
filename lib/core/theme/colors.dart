b024409a54d9068718603e688836976c05dff6b9
alaa7041 <a.hassan7041@su.edu.eg>
2025-09-29 21:19:26 +0300

    finish collecting colors of the all screens

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -1,7 +1,21 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class ColorsManager {
   static const Color primaryColor = Color(0xff2C1E5173);
   static const Color secondaryColor = Color(0xff713649CF);
   static const Color backgroundColor = Color(0xffF5F5F5);
+  static const Color bgFooterColor = Color(0xff16103C);
+  static const Color btnColor = Color(0xff6758FE);
+  static const Color iconColor = Color(0xffD6CCCC);
+  static const Color subTextColor = Color(0xffD6CCCC);
+  static const Color homeTextColor = Color(0xff18153F);
+  static const Color blueColor = Color(0xff5436F8);
+  static const Color textPurpleColor = Color(0xffA49AD8);
+
+  static const Color greyTextColor = Color(0xffA9A9A9);
+  static const Color greyIconColor = Color(0xffAEADB5);
+
 }
+
